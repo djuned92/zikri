2e753f06673afe9b2b2e2b32425c1fd4e4a5146b
djuned92 <ahmaddjunaedi92@gmail.com>
2016-09-28 18:05:46 +0700

    change icon kelola users

--- application/views/template/admin/navbar-sidebar.php
+++ application/views/template/admin/navbar-sidebar.php
@@ -37,7 +37,7 @@
       </li> -->
       <li class="treeview">
         <a href="#">
-            <i class="fa fa-tree"></i> <span>Kelola User</span>
+            <i class="fa fa-users"></i> <span>Kelola User</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
