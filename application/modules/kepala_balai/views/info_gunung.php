6cd75a2c5ad14b48ab7d3f625002a7d2442b7770
djuned92 <ahmaddjunaedi92@gmail.com>
2016-10-14 14:19:26 +0700

    fix info gunung kepala balai

--- application/modules/kepala_balai/views/info_gunung.php
+++ application/modules/kepala_balai/views/info_gunung.php
@@ -3,7 +3,7 @@
     
     <section class="content-header" style="padding-top:0px;">
       <h3>
-        Info Gunung Gede
+        Info Gunung Gede Pangrango
       </h3>
     </section>
 
@@ -41,19 +41,20 @@
                         if($datas->tanggal_pendakian == $r->tanggal_pendakian):
                     ?>
                     <td>
-                      <?php 
+                      <?php
+                        $this->load->model('count_model','count');
+                        $total_pendaki = $this->count->total_pendaki($datas->id_jadwal_pendakian); 
                         if($datas->status_jalur_pendakian != 'Aman')
                         {
                           echo $datas->status_jalur_pendakian;
                         }
                         else
                         {
-                          echo 
-                            "<a href='peserta/index/$datas->id_jadwal_pendakian'>
-                              $datas->kuota
-                            </a>";
+                          $kuota = $datas->kuota - $total_pendaki;
+                          echo $kuota;
                         }
-                      ?>                    </td>
+                      ?>  
+                    </td>
                     <?php endif; endforeach;?>
                   </tr>
                   <?php endforeach; ?>
