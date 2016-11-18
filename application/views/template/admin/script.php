f5a6127142966ab4540077538009e4ef03203635
djuned92 <ahmaddjunaedi92@gmail.com>
2016-11-18 20:23:05 +0700

    fix head script

--- application/views/template/admin/script.php
+++ application/views/template/admin/script.php
@@ -1,9 +1,10 @@
+<!-- jQuery 2.2.3 -->
 <script src="<?=base_url()?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="<?=base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
 <!-- DataTables -->
-<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?=base_url()?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?=base_url()?>assets/dist/js/app.min.js"></script>
 <!-- AdminLTE for demo purposes -->
@@ -11,4 +12,4 @@
 <!-- Bootstrap WYSIHTML5 -->
 <script src="<?=base_url()?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- chained -->
-<script src="https://cdn.jsdelivr.net/jquery.chained/0.9.9/jquery.chained.min.js"></script>
+<script src="<?=base_url()?>assets/plugins/chained/jquery.chained.min.js"></script>
--- application/views/template/petugas_pendakian/head.php
+++ application/views/template/petugas_pendakian/head.php
@@ -6,11 +6,11 @@
 <!-- Bootstrap 3.3.6 -->
 <link rel="stylesheet" href="<?=base_url()?>assets/bootstrap/css/bootstrap.min.css">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="<?=base_url()?>assets/plugins/font-awesome/css/font-awesome.min.css">
 <!-- Ionicons -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="<?=base_url()?>assets/plugins/ionicons/css/ionicons.min.css">
 <!-- DataTables -->
- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+ <link rel="stylesheet" href="<?=base_url()?>assets/plugins/datatables/dataTables.bootstrap.css">
 <!-- Theme style -->
 <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/AdminLTE.min.css">
 <!-- AdminLTE Skins. Choose a skin from the css/skins
